e5515aa02d2b3f83cc5ad31a598778ed4f73839d
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-05-17 22:42:55 -0700

    Arrays with custom types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -6,7 +6,8 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
   type Query {
     greeting(name: String): String!
-    add(a: Float!, b: Float!): Float!
+    add(numbers:[Float!]!): Float!
+    grades: [Int!]!
     me: User!
     post: Post!
   }  
@@ -36,7 +37,15 @@ const resolvers = {
       return 'Hello'
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b
+      if (args.numbers.length === 0) {
+        return 0
+      }
+      return args.numbers.reduce((a, b) => {
+        return a + b
+      })
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 98, 97]
     },
     me() {
       return {
